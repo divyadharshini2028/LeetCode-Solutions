63b735ca6de2997d67fefbe34f11f9d486c99424
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-10 23:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (86.21%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -2,6 +2,18 @@ class Solution {
     public int[] runningSum(int[] nums) {
         int l=nums.length;
         int[] ans=new int[l];
+        ans[0]=nums[0];
+        for(int i=1;i<l;i++){
+            ans[i]=ans[i-1]+nums[i];
+        }
+        return ans;
+    }
+}
+
+/*class Solution {
+    public int[] runningSum(int[] nums) {
+        int l=nums.length;
+        int[] ans=new int[l];
         for(int i=0;i<l;i++){
             int sum=0;
             for(int j=0;j<=i;j++){
@@ -11,4 +23,5 @@ class Solution {
         }
         return ans;
     }
-}
+}*/
+
