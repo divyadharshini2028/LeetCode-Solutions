d58058f7b8013dd112977751df0764dbef7be926
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-11 23:00:56 +0530

    Time: 5 ms (4.68%) | Memory: 45.4 MB (39.78%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -3,7 +3,7 @@ class Solution {
 
         int l=prices.length;
         int[] ans=new int[l];
-        Stack<Integer> stack=new Stack<>();
+        Deque<Integer> stack=new ArrayDeque<>();
 
         for(int i=l-1;i>=0;i--){
            while(!stack.isEmpty() && prices[i]<stack.peek()){
