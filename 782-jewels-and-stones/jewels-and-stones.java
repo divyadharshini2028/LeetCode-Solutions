dcb2439be7c0f772393166404ce6d2c9ce8e46a9
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-13 19:34:55 +0530

    Time: 1 ms (57.90%) | Memory: 43.4 MB (8.63%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int l1=jewels.length();
         int l2=stones.length();
@@ -8,6 +8,26 @@ class Solution {
                 if(jewels.charAt(i)==stones.charAt(j)){
                     count++;
                 }
+            }
+        }
+        return count;
+    }
+}*/
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set=new HashSet<>();
+        int l1=jewels.length();
+        int l2=stones.length();
+        int count=0;
+        for(int i=0;i<l1;i++){
+            if(!set.contains(jewels.charAt(i))){
+                set.add(jewels.charAt(i));
+            }
+        }
+        for(int j=0;j<l2;j++){
+            if(set.contains(stones.charAt(j))){
+                count++;
             }
         }
         return count;
