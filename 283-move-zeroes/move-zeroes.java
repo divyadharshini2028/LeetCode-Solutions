a9b9a1b35edee51d275d458b07c684e3a3806e02
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-14 21:31:47 +0530

    Time: 2 ms (91.70%) | Memory: 47.7 MB (57.40%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public  void moveZeroes(int[] nums) {
+        int l=nums.length;
+        int left=0;
+
+       for(int right=0;right<l;right++){
+        if(nums[right]!=0){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+        }
+       }
+        return;
+    }
+}
