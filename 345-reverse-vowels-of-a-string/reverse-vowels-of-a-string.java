146e20631c278276633f9820422fb2bd90585109
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-14 20:24:16 +0530

    Time: 273 ms (5.07%) | Memory: 48.3 MB (5.36%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverseVowels(String s) {
+        int l=s.length();
+        String ans="";
+        for(int i=0;i<l;i++){
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+                ans=ans+ch;
+            }
+        }
+        int l1=ans.length();
+        int j=0;
+        String res="";
+        for(int i=0;i<l;i++){
+            char ch1=s.charAt(i);
+            if(ch1=='a'||ch1=='e'||ch1=='i'||ch1=='o'||ch1=='u'||ch1=='A'||ch1=='E'||ch1=='I'||ch1=='O'||ch1=='U'){
+               ch1=ans.charAt(l1-1-j);
+               j++;
+               res=res+ch1;
+            }
+            else{
+            res=res+ch1;
+            }
+        }
+        return res;
+    }
+}
