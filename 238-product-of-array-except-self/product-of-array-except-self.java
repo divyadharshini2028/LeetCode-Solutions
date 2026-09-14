8784d0a2eabb51a1095e793f449025c53ee5517d
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-14 19:16:21 +0530

    Time: 2 ms (93.03%) | Memory: 71.9 MB (55.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -22,7 +22,7 @@
         return ans;
     }
 }*/
-
+/*
 class Solution {
     public int[] productExceptSelf(int[] nums) {
       int l=nums.length;
@@ -38,4 +38,23 @@ class Solution {
        }
        return ans;
     }
-}//tc=o(n) sc=o(1)
+}//tc=o(n) sc=o(1)*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int l=nums.length;
+        int[] ans=new int[l];
+        ans[0]=1;
+        for(int i=1;i<l;i++){
+           ans[i]=ans[i-1]*nums[i-1];
+        }
+        int right=1;
+        for(int i=l-1;i>=0;i--){
+           ans[i]=ans[i]*right;
+           right=right*nums[i];
+        }
+        return ans;
+    }
+}
+
+
