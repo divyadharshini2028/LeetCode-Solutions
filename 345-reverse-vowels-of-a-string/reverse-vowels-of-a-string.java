df16723dd33644f99617f6bbe19c02ea4095d29a
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-14 20:31:55 +0530

    Time: 5 ms (20.67%) | Memory: 46.4 MB (79.36%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String reverseVowels(String s) {
         int l=s.length();
         StringBuilder ans=new StringBuilder();
@@ -14,6 +14,39 @@ class Solution {
         for(int i=0;i<l;i++){
             char ch1=s.charAt(i);
             if(ch1=='a'||ch1=='e'||ch1=='i'||ch1=='o'||ch1=='u'||ch1=='A'||ch1=='E'||ch1=='I'||ch1=='O'||ch1=='U'){
+               ch1=ans.charAt(l1-1-j);
+               j++;
+               res.append(ch1);
+            }
+            else{
+            res.append(ch1);
+            }
+        }
+        return res.toString();
+    }
+}*/
+class Solution {
+     public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' ||
+               ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' ||
+               ch == 'O' || ch == 'U';
+    }
+    public String reverseVowels(String s) {
+        int l=s.length();
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<l;i++){
+            char ch=s.charAt(i);
+            if(isVowel(ch)){
+                ans.append(ch);
+            }
+        }
+        int l1=ans.length();
+        int j=0;
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<l;i++){
+            char ch1=s.charAt(i);
+            if(isVowel(ch1)){
                ch1=ans.charAt(l1-1-j);
                j++;
                res.append(ch1);
