5247f091dd3dece1061666f60ad20143904381be
Divya dharshini S <divyasaravanan2028@gmail.com>
2026-09-23 22:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (30.79%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=nums.length;
+        int left=0;
+        int right=l-1;
+        
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]<target){
+               left=mid+1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else if(nums[mid]==target){
+                return mid;
+            }
+        }
+        return -1;
+    }
+}//Time: O(log n)  Space: O(1)
